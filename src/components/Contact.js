b4e8676fd71ab27f6cc121be945ba481d4413424
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-05-09 13:44:17 -0400

    made fixes to about

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,11 @@
 import img from '../img/IMG_7340-removebg-preview (1).png'
 
 const Contact = () =>{
-    function sendEmail() {
-        window.location.href = "mailto:hmjk99@gmail.com";
-    }
+    const emailAddress = 'hmjk99@gmail.com';
+  
+    const sendEmail = () => {
+      window.location.href = `mailto:${emailAddress}`;
+    };
     return(
         <>
          <section id="Contact">
@@ -16,7 +18,7 @@ const Contact = () =>{
                     <h3>Do not hesitate and please reach out if you have any project in mind where my skills can help. Thank you.</h3>
                     <h3>Email: hmjk99@gmail.com</h3>
                     <div>
-                    <button onclick={sendEmail} className="contact-button open-modal">Contact Me</button>
+                    <button onClick={sendEmail} className="contact-button open-modal">Contact Me</button>
                     </div>
             </div>
             <img id="contact-img" src={img} alt=""/>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,16 @@
+import css from '../img/css-3.svg'
+import react from '../img/react.svg'
+import js from '../img/javascript.svg'
+import next from '../img/nextjs-icon.svg'
+import html from '../img/html-5.svg'
+import tailwind from '../img/tailwindcss-icon.svg'
+import jquery from '../img/jquery.svg'
+import python from '../img/python.svg'
+import django from '../img/django-icon.svg'
+import mongo from '../img/mongodb-icon.svg'
+import sql from '../img/postgresql.svg'
+import node from '../img/nodejs-icon.svg'
+import express from '../img/express.svg'
 
 
 const Skills = () =>{
@@ -16,25 +29,25 @@ const Skills = () =>{
                 </div>
                 <div className="skills-info">
                     <div className="skills-detail js bx-tada-hover">
-                        <h4>JavaScript</h4>
+                        <img className="skill-img" src={js}/>
                     </div>
                     <div className="skills-detail jquery bx-tada-hover">
-                        <h4>Jquery</h4>
+                        <img className="skill-img" src={jquery}/>
                     </div>
                     <div className="skills-detail react bx-tada-hover">
-                        <h4>React</h4>
+                        <img className="skill-img" src={react}/>
                     </div>
                     <div className="skills-detail next bx-tada-hover">
-                        <h4>Next.js</h4>
+                        <img className="skill-img" src={next}/>
                     </div>
                     <div className="skills-detail tailwind bx-tada-hover">
-                        <h4>Tailwind CSS</h4>
+                        <img className="skill-img" src={tailwind}/>
                     </div>
                     <div className="skills-detail html bx-tada-hover">
-                        <h4>HTML</h4>
+                        <img className="skill-img" src={html}/>
                     </div>
                     <div className="skills-detail css bx-tada-hover">
-                        <h4>CSS</h4>
+                        <img className="skill-img" src={css}/>
                     </div>
                 </div>
             </div>
@@ -47,22 +60,22 @@ const Skills = () =>{
                 </div>
                 <div class="skills-info">
                     <div class="skills-detail python bx-tada-hover">
-                        <h4>Python</h4>
+                        <img className="skill-img" src={python}/>
                     </div>
                     <div class="skills-detail django bx-tada-hover">
-                        <h4>Django</h4>
+                        <img className="skill-img" src={django}/>
                     </div>
                     <div class="skills-detail node bx-tada-hover">
-                        <h4>Node.js</h4>
+                        <img className="skill-img" src={node}/>
                     </div>
                     <div class="skills-detail mongo bx-tada-hover">
-                        <h4>MongoDB</h4>
+                        <img className="skill-img" src={mongo}/>
                     </div>
                     <div class="skills-detail express bx-tada-hover">
-                        <h4>Express.js</h4>
+                        <img className="skill-img" src={express}/>
                     </div>
                     <div class="skills-detail sql bx-tada-hover">
-                        <h4>Postgresql</h4>
+                        <img className="skill-img" src={sql}/>
                     </div>
                 </div>
                 
