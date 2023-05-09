2bee8a6ba3704df9951fe843e9205f14e36afabf
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-05-09 12:46:48 -0400

    made fixes to about

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,12 @@ const About = () =>{
                 <h1 class="section-title">About Me</h1>
                 <div id="about-p">
                     <img src={img}/>
-                    <p>I am a passionate software developer with a background in exercise science. While my previous experience in the field allowed me to develop a deep understanding of the human body and the science behind movement, my passion for software development has opened up a whole new world of problem-solving and innovation.</p>
-                    <p>My positive, collaborative nature and strong problem-solving skills make me an asset to any project. I approach challenges with enthusiasm and enjoy working with others to bring fresh perspectives to the table. </p>
+                    <p>I am a passionate software developer with a background in <span className='emphasis'>exercise science</span>. While my previous experience in the field allowed me to develop a deep understanding of the human body and the science behind movement, my passion for software development has opened up a <span className='emphasis'>whole new world</span> of problem-solving and innovation.</p>
+                    <p>My positive, collaborative nature and strong problem-solving skills make me an asset to any project. I approach challenges with enthusiasm and enjoy working with others to bring <span className='emphasis'>fresh perspectives</span> to the table. </p>
                 </div>
                 <div id="fun-fact">
                     <h3>Fun Fact:</h3>
-                    <p>During my bootcamp, I was voted as the "Eagle Eye" for my ability to catch even the tiniest of mistakes, and "Code Wrangler" for my knack for solving the toughest problems.</p>
+                    <p>During my bootcamp, I was voted as the <span className='emphasis'>"Eagle Eye"</span> for my ability to catch even the tiniest of mistakes, and <span className='emphasis'>"Code Wrangler"</span> for my knack for solving the toughest problems.</p>
                 </div>
             </div>
         </section>
